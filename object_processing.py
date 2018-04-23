4cf940822c6dd0067c1b5f7bcd91bc8ffa3c8994
Jemaima <metelkina.anna@bk.ru>
2018-04-23 14:55:39 +0300

    in coordinate space works

--- object_processing.py
+++ object_processing.py
@@ -6,24 +6,23 @@ from mpl_toolkits.mplot3d import Axes3D
 deltaT_counter = 20 * 10 ** (-9)  # s
 w_rad = 120 * np.pi
 
-initial_object_pos = [0, 0, 4]  # x,y,z
+initial_object_pos = np.array([0, 0, 1000], np.float32)  # x,y,z , mm
 
+img_size = (int(np.pi / w_rad / deltaT_counter), int(np.pi / w_rad / deltaT_counter))
 
-img_size = (int(np.pi / w_rad/deltaT_counter), int(np.pi / w_rad/deltaT_counter))
+cameraMatrix = np.array([[1, 0, 0],
+                         [0, 1, 0],
+                         [0, 0, 1]
+                         ], dtype=np.float64)
 
-cameraMatrix = np.float64([[1, 0, int(np.pi / w_rad/deltaT_counter/2)],
-                           [0, 1, int(np.pi / w_rad/deltaT_counter)/2],
-                           [0, 0, 1]
-                          ], dtype = np.float64)
-
-dist_coefs = np.float64([0, 0, 0, 0,0])
+dist_coefs = np.float64([0, 0, 0, 0, 0])
 FILE_NAME = 'object.txt'
 
 
 # z - camera axix
 
 
-def get_object_from_file(filename=FILE_NAME):
+def get_object_from_file(filename=FILE_NAME,  initial_transform_vector=initial_object_pos):
     f = open(filename, 'r')
     object_transform = np.float64(f.readline()[:-2].split(',')).astype(float)
     object_rotation = np.float64(f.readline()[:-2].split(',')).astype(float)
@@ -31,7 +30,7 @@ def get_object_from_file(filename=FILE_NAME):
     object_points = np.zeros([object_n_points, 3])
     for i in range(object_n_points):
         object_points[i] = np.float64(f.readline()[:-2].split(',')).astype(float)
-    return TrackedObject(object_points, object_rotation, object_transform)
+    return TrackedObject(50*object_points, object_rotation, object_transform + initial_transform_vector)
 
 
 def generate_rotation_matrix_from_euler_angles(euler_angles):
@@ -60,12 +59,14 @@ def generate_rotation_matrix_from_euler_angles(euler_angles):
 
 class TrackedObject(object):
     def __init__(self, UVW_points,
-                 rotation_vector=np.array([0.0, 0.0, 0.0]), tranform_vector=np.array([0.0, 0.0, 0.0])):
-        UVW_points = np.array(UVW_points)
-        self.UVW_points = UVW_points  # UVW_points[UVW_points[:, 2].argsort()]
+                 initial_rotation_vector=np.array([0.0, 0.0, 0.0]),
+                 initial_transform_vector=np.array([0.0, 0.0, 0.0])
+                 ):
+
+        self.UVW_points = np.array(UVW_points, np.float32)  # UVW_points[UVW_points[:, 2].argsort()]
         self.n_UVW_points = len(UVW_points)
-        self.initial_rotation_vector = np.array(rotation_vector)
-        self.initial_transform_vector = np.array(tranform_vector)
+        self.initial_rotation_vector = np.array(initial_rotation_vector)
+        self.initial_transform_vector = np.array(initial_transform_vector)
 
         # transform parameters
         self.scale = 1.0
@@ -76,135 +77,116 @@ class TrackedObject(object):
         # transform initial object
         self.transformed_UVW_points = None
         # determine visible points and convert them into angles
-        self.visible_points = None
+        self.angle_points_projection = None
         self.n_visible_points = 0
+        self.points_projection = None
         self.visible_point_coordinates = None
 
         # cv2.getRotationMatrix2D([0,0],15,2)
 
-    def set_transformation(self, rotation_vector=None, transform_vector=None):
+    def set_transformation(self, rotation_vector=None, transform_vector=None, noise_scale =0.0):
         self.rotation_vector = np.float64(rotation_vector)
         self.transform_vector = np.float64(transform_vector)
+
         self.rotation_matrix = generate_rotation_matrix_from_euler_angles(self.rotation_vector)
-        self.transformed_UVW_points = np.dot(self.UVW_points, self.rotation_matrix)+transform_vector
-        #
-        self.visible_points = self._find_visible_points() # all points now
-        self.n_visible_points = len(self.visible_points)
-        self.visible_point_coordinates = self._determine_angles_of_visible_points(self.visible_points)
-        self.solvePnP()
+
+        self.transformed_UVW_points = np.dot(self.UVW_points, self.rotation_matrix) \
+                                      + transform_vector + self.initial_transform_vector
+
+        # self.angle_points_projection = self.points2angles(self.transformed_UVW_points, True)
+        self.points_projection = np.squeeze(cv2.projectPoints(self.UVW_points,
+                                                              self.rotation_vector / 180 * np.pi,
+                                                              self.transform_vector + self.initial_transform_vector,
+                                                              cameraMatrix,
+                                                              dist_coefs)[0], 1)
+
+        self.points_projection_noise = self.points_projection +\
+                                       np.random.random(self.points_projection.shape) * \
+                                       noise_scale * (self.points_projection.max() - self.points_projection.min())
+
+        # self.visible_point_coordinates = self.points2angles(self.transformed_UVW_points, False)
+        # self.solvePnP()
         # cv2.solvePnP(self.UVW_points , self.angle_coordinates_of_visible_points, cameraMatrix, dist_coefs)
         # self.uv_ideal_points = self.get_projection()
 
+    #TODO
     def _find_visible_points(self):
-        fig = plt.figure(figsize=(10, 5))
-        ax = fig.add_subplot(121, projection='3d')
-        ax2 = fig.add_subplot(122, projection='3d')
-        ax.scatter(self.UVW_points.T[0],
-                   self.UVW_points.T[1],
-                   self.UVW_points.T[2],
-                   zdir='z')
-        ax.set_title('initial')
-        ax2.scatter(self.transformed_UVW_points.T[0],
-                    self.transformed_UVW_points.T[1],
-                    self.transformed_UVW_points.T[2],
-                    zdir='z')
-        ax2.set_title('transformed')
-        plt.suptitle('Object 3D coordinates')
-
-        mean_z = np.mean(self.transformed_UVW_points.T[2])
-        visible_points = self.transformed_UVW_points #[self.transformed_UVW_points.T[2] <= mean_z]
-
-        # left = self.transformed_UVW_points[np.argmin(self.transformed_UVW_points.T[0])]
-        # right = self.transformed_UVW_points[np.argmax(self.transformed_UVW_points.T[0])]
-        # top = self.transformed_UVW_points[np.argmax(self.transformed_UVW_points.T[1])]
-        # bottom = self.transformed_UVW_points[np.argmin(self.transformed_UVW_points.T[1])]
-        # center_v = (top + bottom)/2
-        # center_h = (left + right) / 2
-        # center = [center_v[0],center_h[1]]
-        #
-        # visible_points = np.array([left, top, right,bottom])
-        # for p in self.transformed_UVW_points:
-        #     if not any([all(np.equal(p,pp)) for pp in visible_points]):
-        #
-        #         # fig = plt.figure()
-        #         # ax = fig.add_subplot(111, projection='3d')
-        #         # ax.scatter(self.transformed_UVW_points.T[0],
-        #         #            self.transformed_UVW_points.T[1],
-        #         #            self.transformed_UVW_points.T[2],
-        #         #            zdir='z')
-        #         # ax.scatter(self.UVW_points.T[0],
-        #         #            self.UVW_points.T[1],
-        #         #            self.UVW_points.T[2],
-        #         #            zdir='z')
-        #         # ax.scatter(p[0],
-        #         #            p[1],
-        #         #            p[2],
-        #         #            zdir='z', s=40)
-        #         # plt.plot([left[0], right[0]],
-        #         #          [left[1], right[1]],
-        #         #          [left[2], right[2]])
-        #         # plt.plot([top[0], bottom[0]],
-        #         #          [top[1], bottom[1]],
-        #         #          [top[2], bottom[2]])
-        #         #
-        #         # plt.show()
-        #
-        #         # left_top
-        #         if p[0] <= center[0] and p[1] >= center[1]:
-        #             projection = left + (top-left)*(top-p)[1]/(top-left)[1]
-        #             if p[2] < projection[2]:
-        #                 visible_points = np.append(visible_points, [p],axis=0)
-        #         # left_bottom
-        #         if p[0] <= center[0] and p[1] <= center[1]:
-        #             projection = left + (bottom-left)*(bottom-p)[1]/(bottom-left)[1]
-        #             if p[2] < projection[2]:
-        #                 visible_points = np.append(visible_points, [p],axis=0)
-        #         # right_top
-        #         if p[0] >= center[0] and p[1] >= center[1]:
-        #             projection = right + (top-right)*(top-p)[1]/(top-right)[1]
-        #             if p[2] < projection[2]:
-        #                 visible_points = np.append(visible_points, [p],axis=0)
-        #         # right_bottom
-        #         if p[0] >= center[0] and p[1] <= center[1]:
-        #             projection = right + (bottom-right)*(bottom-p)[1]/(bottom-right)[1]
-        #             if p[2] < projection[2]:
-        #                 visible_points = np.append(visible_points, [p],axis=0)
+        visible_points = self.transformed_UVW_points  # [self.transformed_UVW_points.T[2] <= mean_z]
         return visible_points
 
-    def _determine_angles_of_visible_points(self, points):
-        angle_coordinates = np.zeros([len(points), 2])
-        for i, p in enumerate(points):
-            angle_coordinates[i] = [np.arctan(p[0] / p[2])+np.pi/2, np.arctan(p[1] / p[2])+np.pi/2]
+    def points2angles(self, points, with_z=False):
+        if with_z:
+            angle_coordinates = np.zeros([len(points), 3])
+            for i, p in enumerate(points):
+                angle_coordinates[i] = [np.arctan(p[0] / p[2]) + np.pi / 2, np.arctan(p[1] / p[2]) + np.pi / 2, p[2]]
+
+        else:
+            angle_coordinates = np.zeros([len(points), 2])
+            for i, p in enumerate(points):
+                angle_coordinates[i] = [np.arctan(p[0] / p[2]) + np.pi / 2, np.arctan(p[1] / p[2]) + np.pi / 2]
+                # angle_coordinates = angle_coordinates/ w_rad / deltaT_counter
+
+        # angle_coordinates[:, :2] = angle_coordinates[:, :-1] / w_rad / deltaT_counter
+        return angle_coordinates  # * 180 / np.pi
+
+    def angles2points(self, angles):
+        if angles.shape[1] == 2:
+            return None
+
+        points = np.zeros([len(angles), 3])
+        for i, p in enumerate(angles):
+            points[i] = np.tan(p * deltaT_counter * w_rad - np.pi / 2) * p[2]
         plt.figure(figsize=(6, 6))
-        plt.scatter(angle_coordinates.T[0] * 180 / np.pi, angle_coordinates.T[1] * 180 / np.pi)
+        plt.scatter(points.T[0], points.T[1])
         plt.title('angle object projection')
-        return (angle_coordinates /w_rad/deltaT_counter).astype(int) #* 180 / np.pi
-
-    def solvePnP(self):
-        a = cv2.solvePnP(self.UVW_points, self.visible_point_coordinates.astype(np.float32), cameraMatrix,
-                         dist_coefs, None, None, False, cv2.SOLVEPNP_ITERATIVE)
-
-        return a
-
-    def add_noise_to_uv_points(self, noise_range):
-        pass
+        return points
 
 
-if __name__=='__main__':
-
+if __name__ == '__main__':
     coord = [[-1, -1, -1],
-             [-1, 1, -1],
-             [1, -1, -1],
-             [1, 1, -1],
-             [-1, -1, 1],
-             [-1, 1, 1],
-             [1, -1, 1],
-             [1, 1, 1],
-             ] * np.random.random([8, 3]) / 10
-    cube = TrackedObject(coord)
+             [-1,  1, -1],
+             [ 1, -1, -1],
+             [ 1,  1, -1],
+             [-1, -1,  1],
+             [-1,  1,  1],
+             [ 1, -1,  1],
+             [ 1,  1,  1],
+             ]  # * np.random.random([8, 3]) / 10
+    cube = TrackedObject(coord, initial_transform_vector=initial_object_pos)
+    cube = get_object_from_file(initial_transform_vector=initial_object_pos)
 
-    cube = get_object_from_file()
-    cube.set_transformation([0, 0, 0], initial_object_pos)
-    [_,r,t] = cube.solvePnP()
+    # cube = get_object_from_file()
+    cube.set_transformation([120, 70, 45], [0, 0, 0], 0.00001)
 
-    cube.set_transformation(r.flatten()*180/np.pi,t.flatten())
+    fig = plt.figure(figsize=(10, 5))
+    ax = fig.add_subplot(121, projection='3d')
+    ax2 = fig.add_subplot(122, projection='3d')
+    ax.scatter(cube.UVW_points.T[0],
+               cube.UVW_points.T[1],
+               cube.UVW_points.T[2],
+               zdir='z')
+    ax.set_title('initial')
+    ax2.scatter(cube.transformed_UVW_points.T[0],
+                cube.transformed_UVW_points.T[1],
+                cube.transformed_UVW_points.T[2],
+                zdir='z')
+    ax2.set_title('transformed')
+    plt.suptitle('Object 3D coordinates')
+
+    plt.figure(figsize=(6, 6))
+    plt.scatter(cube.points_projection.T[0], cube.points_projection.T[1])
+    plt.title('object projection through cv2.projectPoints')
+    plt.show()
+
+    # cube.points_projection_noise = cube.points_projection + np.random.random(cube.points_projection.shape) / 10
+    _, r_restored, t_restored = cv2.solvePnP(cube.UVW_points,
+                                             cube.points_projection_noise.astype(np.float32),
+                                             cameraMatrix,
+                                             dist_coefs,
+                                             tvec=initial_object_pos,
+                                             flags = cv2.SOLVEPNP_ITERATIVE)
+    r_restored, t_restored = np.squeeze(r_restored), np.squeeze(t_restored)/6
+    print('R: \t\t\t', ", ".join("%.2f" % f for f in (r_restored / np.pi * 180)))
+    print('R initial: \t', ", ".join("%.2f" % f for f in cube.rotation_vector))
+    print('T: \t\t\t', ", ".join("%.3f" % f for f in t_restored / np.pi * 180))
+    print('T initial: \t', ", ".join("%.2f" % f for f in cube.transform_vector + cube.initial_transform_vector))
